dd45daf60a2e0b22b57375e94f436c7095ff3a3d
taniMoran <tanimoran@hotmail.com>
2024-06-03 18:36:49 -0300

    kitpreparado

--- Scripts/KitsPreparados.js
+++ Scripts/KitsPreparados.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-alert('hola')
+    $('.categoryItem').click(function(){
+        var catProduct = $(this).attr('category');
+        // OCULTANDO PRODUCTOS
+        $('.product-item').hide();
+
+        // MOSTRANDO PRODUCTOS
+        $('.product-item[category="'+catProduct+'"]').show();   
+
+
+    });
+
+    $('.categoryItem[category="all"]').click(function(){
+
+        $('.product-item').show();
+
+    })
+
 });
