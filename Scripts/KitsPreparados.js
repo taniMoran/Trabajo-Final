0de4c90412dca809db5f9d3f73cba45f99d5c7b6
taniMoran <tanimoran@hotmail.com>
2024-06-03 19:38:55 -0300

    holis

---
+++ Scripts/KitsPreparados.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.categoryItem').click(function(){
+        var catProduct = $(this).attr('category');
+        // OCULTANDO PRODUCTOS
+        $('.product-item').hide();
+
+        // MOSTRANDO PRODUCTOS
+        $('.product-item[category="'+catProduct+'"]').show();   
+
+
+    });
+
+    $('.categoryItem[category="all"]').click(function(){
+
+        $('.product-item').show();
+
+    })
+
+});
