e374c47c46058ceaa0e2b432dbd5587533c65d7f
tani <tanimoran@hotmail.com>
2024-06-18 00:04:26 -0300

    ultimoo

--- Scripts/KitsPreparados.js
+++ Scripts/KitsPreparados.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $('.categoryItem').click(function(){
-        var catProduct = $(this).attr('category');
-        // OCULTANDO PRODUCTOS
-        $('.product-item').hide();
-
-        // MOSTRANDO PRODUCTOS
-        $('.product-item[category="'+catProduct+'"]').show();   
-
-
-    });
-
-    $('.categoryItem[category="all"]').click(function(){
-
-        $('.product-item').show();
-
-    })
-
-=======
 $(document).ready(function() {
     function filterProducts(category) {
         if (category === 'all') {
@@ -35,5 +15,4 @@ $(document).ready(function() {
         var catProduct = $(this).attr('category');
         filterProducts(catProduct);
     });
->>>>>>> d077e167c702b43c8569bece54eb938d6a141908
 });
